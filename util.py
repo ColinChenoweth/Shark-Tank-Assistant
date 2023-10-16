9601758e962429df9d9ae3ca1d18465f6321f5b9
ColinChenoweth <112006905+ChenowethColin@users.noreply.github.com>
2023-10-16 18:20:39 -0400

    comments for util

--- util.py
+++ util.py
@@ -1,4 +1,8 @@
 import numpy as np
 
 def get_data():
+    #features: industry, pitchers gender, pitchers city, pitchers state, pitchers average age, US Viewwership, Original Ask Amount, Original Offered Equity, Valuation Requested
+    #classification: got deal
+    #return x_train, y_train, x_test, y_test
+    #make sure to set random seed and shuffle (so shuffles the same everytime
     return
