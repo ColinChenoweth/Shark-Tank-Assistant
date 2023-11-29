82424b279569575884d781a82050ccdda113d560
Sahil Patel <patelsahil@outlook.com>
2023-11-28 23:53:51 -0500

    port change

--- app.py
+++ app.py
@@ -6,7 +6,13 @@ app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def index():
-    
+    # Specify the path to the R script in the 'Shiny' folder
+    r_script_path = os.path.join('Shiny', 'my_app.R')
+    r_script_path1 = os.path.join('Shiny', 'InvestmentPerSharkapp.R')
+
+    # Run the R script
+    subprocess.run(["Rscript", r_script_path])
+    subprocess.run(["Rscript", r_script_path1])
 
     # Render the HTML template that includes the graph
     return render_template('index.html')
